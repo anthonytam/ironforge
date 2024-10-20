dfe691c4d7895c165ceda80951b04d7492bcd9c3
Kennedy Bushnell <kennedybushnell@kamilon.com>
2024-10-19 23:56:02 -0700

    use Ok

--- src/world_of_warcraft/character.rs
+++ src/world_of_warcraft/character.rs
@@ -3,22 +3,12 @@ use anyhow::Result;
 
 impl WorldOfWarcraftClient {
     pub async fn get_character_equipment_summary(&self, realm_slug: &str, character_name: &str) -> Result<CharacterEquipmentSummary> {
-        //let response_result = self.client
-                                //.send_request(format!("/profile/wow/character/{}/{}/equipment", realm_slug, character_name), "profile-us")
-                                //.await;
-                            //println!("{response_result:?}");
                             let response_result = self.client
-                                .send_request(format!("/profile/wow/character/{}/{}/equipment", realm_slug, character_name), "profile-us")
-                                .await;
-
-                                let response_result = response_result
-                                .json()
-                                .await;
-
-                            response_result.map_err(anyhow::Error::from)
-        // match response_result {
-        //     Ok(response) => Ok(response),
-        //     Err(e) => Err(anyhow::Error::new(e)) //panic!("Failed to get a response. {:?}", e)
-        // }
+                                .send_request(format!("/profile/wow/character/{}/{}/equipment", realm_slug, character_name), "profile")
+                                .await
+                                .json::<CharacterEquipmentSummary>()
+                                .await?;
+                            
+                            Ok(response_result)
     }
 }
